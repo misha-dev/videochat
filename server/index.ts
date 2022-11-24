f742f295d39c05ee27afcbfbb274d440abd3220f
Misha-dev <shabatinmichael@mail.ru>
2022-11-24 23:54:18 +0500

    fixed socket.io bug: misspelled word

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,25 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
+const port = process.env.PORT || 3001;
 
 const app: Express = express();
 const server = http.createServer(app);
-const port = process.env.PORT || 3001;
+
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(`disconnect ${socket.id}`);
+  });
+});
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
